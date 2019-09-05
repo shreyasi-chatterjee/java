9347bdcf98532c9be8d5e3302493b3f5a9956383
shreyasi-chatterjee <53420894+shreyasi-chatterjee@users.noreply.github.com>
2019-09-05 09:56:58 +0530

    Add files via upload

---
+++ 05-09-2019/mypack/methods.java
@@ -0,0 +1,17 @@
+package mypack;
+
+public class methods
+{
+  public static int add(int x,int y)
+  {
+    return x+y;
+  }
+  public static int min(int a,int b)
+  {
+    return a-b;
+  }
+  public static int product(int a,int b)
+  {
+    return a*b;
+  }
+}
