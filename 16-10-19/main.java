9e0c20456cec4669290a3979acac5109951a3695
shreyasi-chatterjee <53420894+shreyasi-chatterjee@users.noreply.github.com>
2019-10-16 09:53:41 +0530

    Add files via upload

---
+++ 16-10-19/main.java
@@ -0,0 +1,29 @@
+class MyThread extends Thread{
+  String message;
+  int sleep;
+
+  MyThread(String a,int b){
+    message=a;
+    sleep=b;
+  }
+  public void run(){
+    for(int i=0;i<100;i++){
+      System.out.println(message);
+      try{
+        Thread.sleep(sleep);
+      }
+      catch(Exception e){
+        System.out.println(e.getMessage());
+      }
+    }
+  }
+}
+class main{
+  public static void main(String args[]){
+
+    MyThread a = new MyThread("BCA",1000);
+    MyThread b = new MyThread("Amity",1000);
+    a.start();
+    b.start();
+  }
+}
